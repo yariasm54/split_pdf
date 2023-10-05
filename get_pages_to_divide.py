773fbf494c41bf935cf5c844f01609d4bee431e8
yariasm54 <32036752+yariasm54@users.noreply.github.com>
2023-10-05 06:52:56 -0500

    Create get_pages_to_divide.py

---
+++ get_pages_to_divide.py
@@ -0,0 +1,22 @@
+import re
+import PyPDF2
+
+def get_pages(pdf_file):
+    expresion_regular = r"ID\.(\d+)ProcesodeSelección"
+    paginas_de_division = []
+    ID_por_pagina = {}
+    numero = 0
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    palabras_a_buscar = ["Proyectó:"]
+    for pagina_num, pagina in enumerate(pdf_reader.pages):
+        contenido = pagina.extract_text()
+        texto_limpio = contenido.replace("\n", "").replace(" ", "")
+        coincidencia = re.search(expresion_regular, texto_limpio)
+        if coincidencia:
+            numero = coincidencia.group(1)
+        for palabra in palabras_a_buscar:
+            if palabra in contenido:
+                paginas_de_division.append(pagina_num + 1)
+                ID_por_pagina[pagina_num + 1] = numero
+                numero = 0
+    return paginas_de_division, ID_por_pagina
