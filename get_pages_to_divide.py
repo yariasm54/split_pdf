43b1af117b2d6633208e926bbcc5e6e86c8287c1
yariasm54 <32036752+yariasm54@users.noreply.github.com>
2024-02-01 15:46:08 -0500

    Update get_pages_to_divide.py

--- get_pages_to_divide.py
+++ get_pages_to_divide.py
@@ -5,10 +5,10 @@ import re
 import PyPDF2
 
 def get_pages(pdf_file):
-    # expresion_regular = r"ID\.(\d+)ProcesodeSelección"
+    expresion_regular = r"ID\.(\d+)ProcesodeSelección"
     # expresion_regular = r"porpartedelaspirante(\d+)\,inscritoalempleocon"
     # expresion_regular = r"AUTONo\.(\d+)DE2024"
-    expresion_regular = r"aspirante\:(\d+)ProcesodeSelección"
+    # expresion_regular = r"aspirante\:(\d+)ProcesodeSelección"
     paginas_de_division = []
     ID_por_pagina = {}
     numero = 0
