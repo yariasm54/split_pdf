d8df0287b408f0cf0c3331f8fb4f63d9d3af10f8
yariasm54 <32036752+yariasm54@users.noreply.github.com>
2024-03-05 22:59:49 -0500

    Update get_pages_to_divide.py

--- get_pages_to_divide.py
+++ get_pages_to_divide.py
@@ -8,7 +8,7 @@ def get_pages(pdf_file):
     # expresion_regular = r"ID\.(\d+)ProcesodeSelección"
     # expresion_regular = r"porpartedelaspirante(\d+)\,inscritoalempleocon"
     # expresion_regular = r"AUTONo\.(\d+)DE2024"
-    expresion_regular = r"aspirante\:(\d+)\,"
+    expresion_regular = r"aspirante(\d+)\,"
     paginas_de_division = []
     ID_por_pagina = {}
     numero = 0
